ac72d04a7de3e2151d8d6c9070afeebb199f6077
장정운 <jjangnotjang@gmail.com>
2025-03-12 23:07:00 +0900

    design: 그래프 퍼블리싱 배치

--- src/components/Graphs/Graphs.tsx
+++ src/components/Graphs/Graphs.tsx
@@ -1,3 +1,5 @@
+"use client"; // 클라이언트 컴포넌트 지정
+
 import React from "react";
 import {
   LineChart,
@@ -10,62 +12,22 @@ import {
 } from "recharts";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
+  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
+  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
 ];
 
-export default function App() {
+const Graphs = () => {
   return (
     <LineChart
-      width={1200}
-      height={400}
+      width={950}
+      height={500}
       data={data}
-      margin={{
-        top: 10,
-        right: 40,
-        left: 30,
-        bottom: 10,
-      }}
+      margin={{ top: 5, right: 20, left: 15, bottom: 5 }}
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
@@ -78,16 +40,18 @@ export default function App() {
         type="monotone"
         dataKey="pv"
         stroke="#8884d8"
-        strokeWidth={4}
-        activeDot={{ r: 8 }}
+        strokeWidth={3}
+        activeDot={{ r: 6 }}
       />
       <Line
         yAxisId="right"
         type="monotone"
         dataKey="uv"
         stroke="#82ca9d"
-        strokeWidth={4}
+        strokeWidth={3}
       />
     </LineChart>
   );
-}
+};
+
+export default Graphs;
